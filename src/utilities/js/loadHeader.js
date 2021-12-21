e9a619f947cad73904a94b14c0b8e0634ef36d7a
tinoswe <you@example.com>
2021-12-21 13:36:52 +0100

    logo update

--- src/utilities/js/loadHeader.js
+++
@@ -1,1 +0,0 @@
-fetch('/html/pageElements/header.html').then(res => res.text()).then(text => {document.querySelector("#header").innerHTML = text;})
--- src/utilities/js/showNavbar.js
+++
@@ -1,1 +0,0 @@
-fetch('/html/pageElements/navbar.html').then(res => res.text()).then(text => {document.querySelector("#nav-placeholder").innerHTML = text;})
